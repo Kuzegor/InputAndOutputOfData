8a4982f46c44a867784ada978cd677aeecb9b0c9
Kuzegor <kuznetcov03@inbox.ru>
2022-11-18 23:52:27 +0300

    commit

---
+++ Ввод и вывод данных/Дележ яблок/Дележ_яблок.py
@@ -0,0 +1,5 @@
+n = int(input("input number of students: "))
+k = int(input("input number of apples: "))
+print("apples per student: ",k//n)
+print("apples left: ",k%n)
+
---
+++ Ввод и вывод данных/Электронные часы/Электронные_часы.py
@@ -0,0 +1,2 @@
+minutes = int(input("input number of minutes: "))
+print(minutes//60,":",minutes%60)
